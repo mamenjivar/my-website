26843200a982b0415f63ca81d7bd399ffdc94cf7
Miguel <menjivarmiguel221@gmail.com>
2022-02-28 23:14:10 -0800

    updating the homepage with new content

--- miguel-webpage/src/components/Overlay.js
+++ miguel-webpage/src/components/Overlay.js
@@ -1,8 +1,14 @@
 import { Fragment } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+
+// fontawesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe, faFolderOpen, faUser } from "@fortawesome/free-solid-svg-icons"
 import { faLinkedin, faInstagram, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+import "./Overlay.css";
+import pfp from '../utils/img/pfp.jpg';
+
 
 const Overlay = () => {
   return (
@@ -11,23 +17,25 @@ const Overlay = () => {
         <Row >
           <Col>
             <div className="text-center">
-                <h1>Profile Picture</h1>
+                <img id="profile-picture" src={pfp} alt="profile"/>
                 <h1>Miguel Menjivar</h1>
             </div>
           </Col>
           <Col>
-          <div class="text-center">
-                <p>Urban TXT: Teens Exploring Technology</p>
-                <p>Fullstack Web Developer</p>
-                <p>Los Angeles, CA</p>
+          <div class="text-center bordering d-flex flex-column justify-content-around">
+                <h1 >Urban TXT: Teens Exploring Technology</h1>
+                <h1>Fullstack Web Developer</h1>
+                <h1 >Los Angeles, CA</h1>
 
-                <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faGlobe}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
-                <FontAwesomeIcon icon={faFolderOpen}></FontAwesomeIcon>
+                <div className="display-4">
+                  <FontAwesomeIcon className="pr-2" icon={faUser}></FontAwesomeIcon>
+                  <FontAwesomeIcon icon={faGlobe}></FontAwesomeIcon>
+                  <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
+                  <FontAwesomeIcon icon={faInstagram}></FontAwesomeIcon>
+                  <FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon>
+                  <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                  <FontAwesomeIcon icon={faFolderOpen}></FontAwesomeIcon>
+                </div>
             </div>
           </Col>
         </Row>
